96746027f5921ca3356186e29476709cc21e8c64
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-03-26 23:07:55 +0900

    170 Promise chaining

--- 16 Asyncronous Code, Callbacks, Promises/169 Resolving Rejecting with Values/app.js
+++ 16 Asyncronous Code, Callbacks, Promises/169 Resolving Rejecting with Values/app.js
@@ -1,27 +1,24 @@
 const fakeRequest = (url) => {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
-				const pages = {
-					'/users': [
-						{id:1, username:'Bilbo'},
-						{id:5, username:'Esmeralda'}
-					],
-					'/about' : 'This is the about page!'
-				};
-				const data = pages[url]
-				if(data){
-					resolve({status:200, data:data})	
-				} else {
-					reject({ status:404 })
-				}
-		}, 1000)
+			const pages = {
+				'/users': [ { id: 1, username: 'Bilbo' }, { id: 5, username: 'Esmeralda' } ],
+				'/about': 'This is the about page!'
+			};
+			const data = pages[url];
+			if (data) {
+				resolve({ status: 200, data: data });
+			} else {
+				reject({ status: 404 });
+			}
+		}, 1000);
 	});
 };
 
 fakeRequest('/users')
 	.then((res) => {
-		console.log('Status Code', res.status)
-		console.log('Status Code', res.data)
+		console.log('Status Code', res.status);
+		console.log('Status Code', res.data);
 		console.log('Request worked!');
 	})
 	.catch((res) => {
@@ -29,12 +26,10 @@ fakeRequest('/users')
 		console.log('Request failed');
 	});
 
-
-	
-	fakeRequest('/dogs')
+fakeRequest('/dogs')
 	.then((res) => {
-		console.log('Status Code', res.status)
-		console.log('Status Code', res.data)
+		console.log('Status Code', res.status);
+		console.log('Status Code', res.data);
 		console.log('Request worked!');
 	})
 	.catch((res) => {
