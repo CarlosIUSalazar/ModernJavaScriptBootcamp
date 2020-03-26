0076c85b7fe9aa2e6a89ca7c5e2c4b0a9ab91601
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-03-26 21:43:35 +0900

    168 Promises

--- 16 Asyncronous Code, Callbacks, Promises/117 Promises/app.js
+++ 16 Asyncronous Code, Callbacks, Promises/117 Promises/app.js
@@ -1,14 +1,37 @@
-const willGetYouADog = new Promise((resolve,reject) => {
-	const rand = Math.random();
-	if(rand <0.5) {
-		resolve()
-	} else {
-		reject()
-	}
+// const willGetYouADog = new Promise((resolve,reject) => {
+// 	const rand = Math.random();
+// 	if(rand <0.5) {
+// 		resolve()
+// 	} else {
+// 		reject()
+// 	}
+// })
+// willGetYouADog.then(() => {
+// 	console.log("Yay we got a dog")
+// })
+// willGetYouADog.catch(() => {
+// 	console.log("No dog")
+// })
+
+const makeDogPromise = () => {
+//const willGetYouADog = new Promise((resolve, reject) => {
+	return new Promise((resolve,reject) => {	
+		setTimeout(() => {
+		const rand = Math.random();
+		if (rand < 0.5) {
+			resolve();
+		} else {
+			reject();
+		}
+	}, 5000);
+});
+}
+
+makeDogPromise()
+.then(() => {
+	console.log('Yay we got a dog');
 })
-willGetYouADog.then(() => {
-	console.log("Yay we got a dog")
-})
-willGetYouADog.catch(() => {
-	console.log("No dog")
-})
+.catch(() => {
+	console.log('No dog');
+});
+
